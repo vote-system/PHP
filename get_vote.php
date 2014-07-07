11fb0b7823c5db7c58ad9c27882fc24079c92e34
zhaobo1023 <zhaobo1023@gmail.com>
2014-07-07 19:09:20 +0800

    0707

--- get_vote.php
+++ get_vote.php
@@ -12,6 +12,8 @@ $query = "select * from usrinfo where usrname = '".$usrname."'";
 $usrinfo = vote_get_array($query);
 $participant_vote_ids = unserialize($usrinfo["participant_vote_id"]);
 
+
+
 foreach($participant_vote_ids as $vote_id)
 {
 	$query = "select * from vote_info where vote_id = '".$vote_id."'";
@@ -21,14 +23,23 @@ foreach($participant_vote_ids as $vote_id)
 	$vote_preview['vote_id'] = (int)$vote_info['vote_id'];
 	$vote_preview['private'] = $vote_info['private'];
 	$vote_preview['organizer'] = $vote_info['organizer'];
-	$vote_preview['end_time'] = $vote_info['end_time'];
-	$vote_preview['basic_timestamp'] = $vote_info['basic_timestamp'];
-	$vote_preview['vote_timestamp'] = $vote_info['vote_timestamp'];
+	$vote_preview['end_time'] = (double)$vote_info['end_time'];
+	$vote_preview['start_time'] = (double)$vote_info['start_time'];
+	$vote_preview['category'] = $vote_info['category'];
+	$vote_preview['basic_timestamp'] = (int)$vote_info['basic_timestamp'];
+	$vote_preview['vote_timestamp'] = (int)$vote_info['vote_timestamp'];
 
 	$vote_array[] = $vote_preview;
 }
 
-$votes['votes'] = $vote_array;
+if(!$vote_array)
+{
+	$votes['votes'] = NULL;
+}
+else
+{
+	$votes['votes'] = $vote_array;
+}
 echo json_encode($votes,JSON_UNESCAPED_SLASHES);
 
 ?>
